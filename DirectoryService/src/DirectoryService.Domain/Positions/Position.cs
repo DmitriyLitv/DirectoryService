349e4aa66892cef7ffe69388e15ea433ea04455b
Dima <Dimalitv8@gmail.com>
2025-11-01 22:46:42 +0300

    Positions added

---
+++ DirectoryService/src/DirectoryService.Domain/Positions/Position.cs
@@ -0,0 +1,27 @@
+﻿using DirectoryService.Domain.Shared;
+
+namespace DirectoryService.Domain.Positions
+{
+    internal class Position
+    {
+        public Guid Id { get; private set; }
+
+        public PositionName Name { get; private set; } = null!;
+
+        public PositionDescription Description { get; private set; } = null!;
+
+        public int StateCode { get; private set; } = (int)RecordStateCode.IsActive;
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; private set; }
+
+        public Position()
+        {
+            Id = Guid.NewGuid();
+
+            CreatedAt = DateTime.Now;
+            UpdatedAt = CreatedAt;
+        }
+    }
+}
---
+++ DirectoryService/src/DirectoryService.Domain/Positions/PositionName.cs
@@ -0,0 +1,35 @@
+﻿using DirectoryService.Domain.Departments;
+using DirectoryService.Domain.Shared;
+using DirectoryService.Domain.Shared.StringValidators;
+
+namespace DirectoryService.Domain.Positions
+{
+    internal class PositionName : StringHolder, IStringValidatable
+    {
+        private PositionName(string value)
+            : base(value)
+        {
+        }
+
+        public static PositionName Create(string value)
+        {
+            var validator = CreateValidator();
+
+            if (!validator.IsValid(value))
+                return null; // TODO ResultPattern
+
+            return new PositionName(value);
+        }
+
+        public static StringValidatorHandler CreateValidator()
+        {
+            int minLength = 3;
+            int maxLength = 100;
+
+            var isEmptyString = new EmptyStringValidator();
+            var lengthValidator = new LimitedLengthStringValidator(minLength, maxLength);
+
+            return new StringValidatorHandler(isEmptyString, lengthValidator);
+        }
+    }
+}
