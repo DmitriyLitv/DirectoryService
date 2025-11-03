0f9bd5c35fbbcd806d8c907fdafd37251b73eb51
Dima <Dimalitv8@gmail.com>
2025-11-03 14:42:29 +0300

    EF Core installed. DBContext created

---
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/DirectoryServiceDBContext.cs
@@ -0,0 +1,36 @@
+﻿using DirectoryService.Domain.DepartmentLocations;
+using DirectoryService.Domain.DepartmentPositions;
+using DirectoryService.Domain.Departments;
+using DirectoryService.Domain.Locations;
+using DirectoryService.Domain.Positions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Infrastructure.Postgres
+{
+    public class DirectoryServiceDBContext : DbContext
+    {
+        private string _connectionString;
+         
+        public DbSet<Department> Departments => Set<Department>();
+
+        public DbSet<Location> Locations => Set<Location>();
+
+        public DbSet<Position> Positions => Set<Position>();
+
+        public DbSet<DepartmentPosition> DepartmentPosition => Set<DepartmentPosition>();
+
+        public DbSet<DepartmentLocation> DepartmentLocation => Set<DepartmentLocation>();
+
+        public DirectoryServiceDBContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+    }
+}
---
+++ DirectoryService/src/DirectoryService.Infrastructure.Postgres/DirectoryServiceRepository.cs
@@ -0,0 +1,17 @@
+﻿using DirectoryService.Domain.Departments;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Infrastructure.Postgres
+{
+    public class DirectoryServiceRepository
+    {
+        private DirectoryServiceDBContext _context;
+
+        public DirectoryServiceRepository(DirectoryServiceDBContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
